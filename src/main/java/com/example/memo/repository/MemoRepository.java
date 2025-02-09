d555a852ce1dc96b1f4c4058dbfa91127a94950f
Roloya28 <choej9742@naver.com>
2025-02-09 13:47:54 +0900

    second commit

--- src/main/java/com/example/memo/repository/MemoRepository.java
+++ src/main/java/com/example/memo/repository/MemoRepository.java
@@ -6,7 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
-    Long id(Long id);
-
-    List<Memo> id(Long id);
 }
